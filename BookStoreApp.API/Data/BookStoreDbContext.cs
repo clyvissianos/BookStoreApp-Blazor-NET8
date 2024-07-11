addad177168bb788d3e2ec3f520b25765eb15abf
clyvissianos <c.lyvissianos@gmail.com>
2024-07-11 20:12:50 +0300

    Added EF Core and Scaffolded Database

---
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.API.Data;
+
+public partial class BookStoreDbContext : DbContext
+{
+    public BookStoreDbContext()
+    {
+    }
+
+    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC078DC1F727");
+
+            entity.Property(e => e.Bio).HasMaxLength(250);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Books__3214EC07EA25AAB5");
+
+            entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EA1A7E966A").IsUnique();
+
+            entity.Property(e => e.Image)
+                .HasMaxLength(50)
+                .HasColumnName("image");
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(50)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Summary).HasMaxLength(250);
+            entity.Property(e => e.Title).HasMaxLength(50);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.AuthorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Books_ToTable");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,8 +1,12 @@
+using BookStoreApp.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var conString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(conString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
